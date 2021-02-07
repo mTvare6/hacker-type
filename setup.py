8cb758395ef90e337846d32a63b1aaa9f57a93ce
mTvare6 <austin76912@gmail.com>
2021-02-07 15:03:53 +0530

    jeez rushed packing

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+setuptools.setup(
+        name='hacker-type',
+    version='0.1.0',
+    scripts=['bin/hacker-type']
+    author='mTvare',
+    description='CLI based version of https://hackertyper.net/.',
+    install_requires=[
+        'setuptools'
+    ],
+    python_requires='>=3.6'
+)
